f28962e51db59d49eba79c21c6b87be1e892b69e
tanatanJ <152682774+tanatanJ@users.noreply.github.com>
2024-07-22 19:56:55 +0700

    fix size icon

--- src/app/components/Text.tsx
+++ src/app/components/Text.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Text: React.FC = () => {
   return (
     <div className="template-tool">
-      <h2>Template Tool</h2>
+      <h2>Text Tool</h2>
     </div>
   );
 };
